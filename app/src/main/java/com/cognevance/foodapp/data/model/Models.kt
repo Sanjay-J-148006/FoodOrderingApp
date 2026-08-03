8bd2fd39dbc003d20bac7934b6b41ad6fcf73f87
Sanjay Jegadesan <sanjayrjegadesan@gmail.com>
2026-08-03 10:14:56 +0530

    Clean up unused duplicate files

--- app/src/main/java/com/cognevance/foodapp/data/model/Models.kt
+++
@@ -1,4 +0,0 @@
-package com.cognevance.foodapp.data.model
-
-// Duplicate definitions removed. Models are individually defined in:
-// Product.kt, FoodItem.kt, ProductResponse.kt, CartItem.kt, FavoriteItem.kt, User.kt, Order.kt
